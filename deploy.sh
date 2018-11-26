f4e09523b41b7236973d174a250a7824a4e3314b
Ralf Biedert <rb@xr.io>
2018-11-26 22:37:38 +0100

    Ensure we actually check external links on deploy.

--- deploy.sh
+++ deploy.sh
@@ -3,6 +3,12 @@
 # Poor man's deploy script in lack of better infrastructure.
 #
 
+if ! grep "^build_search_index = false$" config.toml > /dev/null
+then
+   echo "'build_search_index' must be enabled in 'config.toml'"
+   exit -1
+fi
+
 zola build
 
 aws s3 cp public s3://cheats.rs/ --recursive
