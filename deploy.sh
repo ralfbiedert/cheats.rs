2f834e4e52105ccfa3f409f43ae642b204ca5a06
Ralf Biedert <rb@xr.io>
2018-11-27 21:02:19 +0100

    Improved deployment flow.

--- deploy.sh
+++ deploy.sh
@@ -3,13 +3,21 @@
 # Poor man's deploy script in lack of better infrastructure.
 #
 
-if ! grep "^check_external_links = true$" config.toml > /dev/null
+TOML_BASE=config.toml
+TOML_DEPLOY=config.deploy.toml
+
+cp $TOML_BASE $TOML_DEPLOY
+
+echo "" >> $TOML_DEPLOY
+echo "check_external_links = true" >> $TOML_DEPLOY
+
+if ! grep "^check_external_links = true$" $TOML_DEPLOY > /dev/null
 then
-   echo "'check_external_links' must be enabled in 'config.toml'"
+   echo "Failed to activate 'check_external_links'."
    exit -1
 fi
 
-zola build
+zola -c $TOML_DEPLOY build
 
 aws s3 cp public s3://cheats.rs/ --recursive
 aws cloudfront create-invalidation --distribution-id E3P5E5G4A4QPL8 --paths "/*"
