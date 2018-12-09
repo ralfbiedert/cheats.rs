5b8e3ba7090d9289ab68b88e404512f1478986f7
Ralf Biedert <rb@xr.io>
2018-12-09 14:54:43 +0100

    Improved sitemap generation date.

--- deploy.sh
+++ deploy.sh
@@ -3,8 +3,11 @@
 # Poor man's deploy script in lack of better infrastructure.
 #
 
+DIST=public
 TOML_BASE=config.toml
 TOML_DEPLOY=config.deploy.toml
+
+NOW=`date +"%Y-%m-%dT%H:%M:%S+02:00"`
 
 cp $TOML_BASE $TOML_DEPLOY
 
@@ -19,5 +22,9 @@ fi
 
 zola -c $TOML_DEPLOY build
 
-aws s3 cp public s3://cheats.rs/ --recursive
+# Update deplyment date in sitemap.xml
+sed -i -e "s/_NOW_/$NOW/g" $DIST/sitemap.xml
+
+
+aws s3 cp $DIST s3://cheats.rs/ --recursive
 aws cloudfront create-invalidation --distribution-id E3P5E5G4A4QPL8 --paths "/*"
