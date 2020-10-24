8b30cc0d8f4a7618dd682e5ea5ddd441ae734ea3
johannst <johannes.stoelp@gmail.com>
2020-10-24 18:36:57 +0200

    docker: use google-chrome instead wkhtmltopdf to generate pdf

--- docker/build.sh
+++ docker/build.sh
@@ -14,12 +14,16 @@ if test -z $zola_url; then
     exit 1
 fi
 
+# Chrome download URL.
+chrome_url=https://dl.google.com/linux/direct/google-chrome-stable_current_amd64.deb
+
 echo "-- info --"
-echo "TAG : $tag"
-echo "ZOLA: $zola_url"
+echo "TAG   : $tag"
+echo "ZOLA  : $zola_url"
+echo "CHROME: $chrome_url"
 echo "----------"
 
-if ! docker build --tag $tag --build-arg ZOLA_URL=$zola_url $script_dir; then
+if ! docker build --tag $tag --build-arg ZOLA_URL=$zola_url --build-arg CHROME_URL=$chrome_url $script_dir; then
     echo "ERR: Failed to build docker image!"
     exit 1
 fi
